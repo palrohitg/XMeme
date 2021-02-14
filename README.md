843da7b911aaba9418fa3922cf3a9b187fae708d
Vikas Pal <rohitprotobit@gmail.com>
2021-02-14 20:50:20 +0530

    readme addded

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# XMeme
-Meme App 
+# :metal:	XMEME 	:snake:	
+![Review-Scrapper](https://socialify.git.ci/palrohitg/reviewscrapper/image?font=Source%20Code%20Pro&forks=1&issues=1&language=1&owner=1&pattern=Brick%20Wall&pulls=1&stargazers=1&theme=Light)
+
+[![HitCount](http://hits.dwyl.com/palrohitg/https://githubcom/palrohitg/reviewscrapper.svg)](http://hits.dwyl.com/palrohitg/https://githubcom/palrohitg/reviewscrapper)
+
+## :pushpin: Introduction 
+
+ReviewScrapper is a web based application which is primarly used to scraps the reviews for a particualar products from Ecommerce websites and save the reviews in the csv format that can be further used for Data-Analysis. ReviewScrapper also allows user to limit the reviews based on the Data requirement and scraps the reviews from the multiple pages.
+:arrow_right: Live :beers:[vikas-review-scrapper](https://vikas-webscrappper-review.herokuapp.com/) :beers:
+
+## :computer: Tech Stack
+    
+* [Flask](https://flask.palletsprojects.com/)
+* [MonogDB](https://www.mongodb.com/)
+* [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/doc)
+* [Python](https://www.python.org/)
+* [Heroku](https://www.heroku.com/)
+* [Bootstrap](https://getbootstrap.com/)
+
+
+## :running_woman: Local Installation Guide : 
+
+1. Clone the repository by using Git Client: 
+
+         git clone https://github.com/palrohitg/reviewscrapper.git
+
+2. Make a virtual environment mandatory: 
+
+         * install pip, pipenv
+         * Python -m venv virtual_env_name
+         * make the virtual env and activate it 
+
+3. Install the Packages: 
+
+        pip install -r requirements.txt
+
+4. Configure the mongoDB not mandatory [mlab](https://mlab.com/) :
+    
+         create Cluster, User Access and Network if need to make changes to db.
+
+5. Run the reviewscrapper and enjoy : 
+
+        python app.py
+
+6. Finally don't forget to put Star :sunglasses:
+
+## :arrow_lower_right:	Outcome/Result:
+  
+  :ballot_box_with_check: Reduces the Customer time by 70% while spending time for checking review of the product.
+
+  :ballot_box_with_check: Helps to Gather information how's customer feels about the specific product and provide better services from business point of view. 
+  
+  :ballot_box_with_check: It can be used for offline Saler(Mobile) who want to upscale their business online. And Figuring out which product their put on their portal which increase the sales.
+
+## :dart:	Scope of Improvement : 
+
+* :blossom: UI can more Interactive. 
+* :blossom: Scrap the reivews from multiple sites.
+* :blossom: More features can be added. 
+* :blossom: Suggestion and feed-Back always welcome.
+
+
+## :iphone:	Running Instance:
+
+
+* :arrow_right: Live :beers:[vikas-review-scrapper](https://vikas-webscrappper-review.herokuapp.com/) :beers:
+* **Note**: Site maybe down I m working :crossed_fingers:
+* **Side Note**: As the flipkart site has different design for different kind of product the review-scrapper not able to fetch the record. we have implement the desing related to electronic items. 
+
+
+## 📜 LICENSE
+
+  [MIT](https://github.com/palrohitg/reviewscrapper/blob/master/LICENSE) 
--- app.py
+++ app.py
@@ -1,73 +1,108 @@
 import pymongo
 from pymongo import MongoClient
 import urllib.parse
-from flask import Flask, render_template, request, jsonify, Response
+from flask import Flask, render_template, request, jsonify, Response, redirect, url_for
 import json
 import os
 import ast
 from flask_cors import CORS, cross_origin
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-# Credentials of Database
+# Local Data-Base Connection
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/XMEME"
+
+# Cloud Data-Base Connection
 username = urllib.parse.quote_plus('root')
 password = urllib.parse.quote_plus("root1234")
-
 url = "mongodb+srv://{}:{}@cluster0.o2yph.mongodb.net/XMEME?retryWrites=true&w=majority".format(
     username, password)
+app.config["MONGO_URI"] = url
 
-cluster = MongoClient(url)
-db = cluster['XMEME']
-meme = db['meme']
+mongo = PyMongo(app)
 
 
-# Get all the meme details
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @cross_origin()
 def home():
-    documents = meme.find({})
-    response = [{item: data[item] for item in data if item != '_id'}
-                for data in documents]
-    return render_template("index.html", result_list=response)
+
+    if request.method == "POST":
+        name = request.form['memeOwner']
+        caption = request.form['memeCaption']
+        url = request.form['memeURL']
+        mongo.db.meme.insert_one({"id": mongo.db.meme.find().count()+1, "name": name,
+                                  "caption": caption, "url": url})
+        return redirect(url_for("home"))
+    else:
+        documents = mongo.db.meme.find({})
+        response = [{item: data[item] for item in data if item != '_id'}
+                    for data in documents]
+        sorted_response = sorted(response, key=lambda i: i['id'], reverse=True)
+        print(sorted_response)
+        return render_template("index.html", result_list=sorted_response)
 
 
-# Post API
+# Get all memes(Endpoint) + create new memes(End`point)
 @app.route("/memes", methods=['GET', 'POST'])
 @cross_origin()
 def create_meme():
     if request.method == "POST":
         content = request.get_json(force=True)
-        meme_url = content['meme_url']
-        meme_owner = content['meme_owner']
-        meme_caption = content['meme_caption']
+        name = content['name']
+        caption = content['caption']
+        url = content['url']
 
-        # Insert into database
-        meme.insert_one({"id ": meme.find().count()+1, "meme_owner": meme_owner,
-                         "meme_caption": meme_caption, "meme_url": meme_url})
+        # Duplicate post exists in the database
+        name_res = mongo.db.meme.count_documents({"name": name})
+        caption_res = mongo.db.meme.count_documents({"caption": caption})
+        url_res = mongo.db.meme.count_documents({"url": url})
+        if(name_res != 0 and caption_res != 0 and url_res != 0):
+            return " dupilcate 409"
+        else:
+            # Instering meme into db query
+            mongo.db.meme.insert_one({"id": mongo.db.meme.find().count()+1, "name": name,
+                                      "caption": caption, "url": url})
+            id = mongo.db.meme.find().count()
+            return {'id': str(id)}, 200
 
-        resp = jsonify('User added successfully!')
-        resp.status_code = 200
-        return resp
-
-    documents = meme.find({})
+    documents = mongo.db.meme.find({})
     response = [{item: data[item] for item in data if item != '_id'}
                 for data in documents]
-    return Response(response=json.dumps(response),
+    return Response(response=json.dumps(response, indent=3),
                     status=200,
                     mimetype='application/json')
 
 
-@app.route("/memes/<id>", methods=["GET"])
+# Update meme by id(Endpoint) + get meme by id(Endpoint)
+@ app.route("/memes/<int:id>", methods=["GET", "PATCH"])
+@ cross_origin()
 def get_meme_by_id(id):
-    documents = meme.find({"id": id})
-    print(documents)
-    for data in documents:
-        print(data)
-    response = [{item: data[item] for item in data if item != '_id'}
-                for data in documents]
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
+
+    if request.method == "PATCH":
+        content = request.get_json(force=True)
+        url = content['url']
+        caption = content['caption']
+        query = {"id": id}
+        new_query = {
+            "$set": {"caption": caption, "url": url}}
+        mongo.db.meme.update_one(query, new_query)
+        return "200"
+
+    else:
+        try:
+            data = mongo.db.meme.find_one({"id": id})
+            response = [{item: data[item] for item in data if item != '_id'}]
+            return Response(response=json.dumps(response, indent=3),
+                            status=200,
+                            mimetype='application/json')
+        except:
+            return "404"
+
+
+@ app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
