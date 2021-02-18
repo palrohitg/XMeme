eeed2dd4084626de7026466ccbc5a95239a9e10f
Vikas Pal <rohitprotobit@gmail.com>
2021-02-18 21:30:19 +0530

    github origin added

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ sudo apt-get install python3
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate 
-pip install -r requirements.txt
+pip install -r requirements.txt 
 
+# Install database file 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
 echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
@@ -17,7 +18,3 @@ echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/
 sudo apt-get update
 
 sudo apt-get install -y mongodb-org-shell
-
-
-
-# Any configuration related commands
